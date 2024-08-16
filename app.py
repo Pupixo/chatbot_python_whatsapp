e50f50bc8e7e4f9b5c3956468c8cf409c19a764e
PEDRO ELIAS ACOSTA GUERRA <acostap@ADPER.HITSS.MX>
2024-08-16 17:32:12 -0500

    actualización se agrego token de acceso

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,request,jsonify,render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 import json
@@ -21,12 +21,12 @@ class Log(db.Model):
 with app.app_context():
     db.create_all()
 
-    prueba1 = Log(text='Mensaje de Prueba 1')
-    prueba2 = Log(text='Mensaje de Prueba 2')
+    # prueba1 = Log(text='Mensaje de Prueba 1')
+    # prueba2 = Log(text='Mensaje de Prueba 2')
 
-    db.session.add(prueba1)
-    db.session.add(prueba2)
-    db.session.commit()
+    # db.session.add(prueba1)
+    # db.session.add(prueba2)
+    # db.session.commit()
 
 def ordenar_por_fecha_y_hora(registros):
     return sorted(registros, key=lambda x: x.fecha_y_hora,reverse=True)
@@ -51,7 +51,34 @@ def agregar_mensajes_log(texto):
 
 
 
-# agregar_mensajes_log(json.dumps("Test1"))
+#
+
+#token de verificación para la configiración
+TOKEN_NOCBOT="NOCBOTCODE"
+
+
+@app.route('/webhook',methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method == 'POST':
+        reponse = recibir_mensajes(request)
+        return reponse
+
+def verificar_token(req):
+    token = req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token ==TOKEN_NOCBOT:
+        return challenge
+    else:
+        return jsonify({'error':'Token Invalido'}),401
+
+def recibir_mensajes(req):
+    req = request.get_json()
+    agregar_mensajes_log(req)
+    return jsonify({'message':'EVENT_RECEIVED'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
